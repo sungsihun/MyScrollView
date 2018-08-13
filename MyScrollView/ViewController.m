b72907be244177a70bda9ea5330efeec829ba3cf
SiHun Sung <sungsihun@gmail.com>
2018-08-13 11:04:56 -0400

    create UIView

--- MyScrollView/ViewController.m
+++ MyScrollView/ViewController.m
@@ -9,6 +9,12 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (nonatomic, weak) UIView *whiteView;
+@property (nonatomic, weak) UIView *redView;
+@property (nonatomic, weak) UIView *greenView;
+@property (nonatomic, weak) UIView *blueView;
+@property (nonatomic, weak) UIView *yellowView;
+
 
 @end
 
@@ -16,14 +22,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    // whiteView
+    UIView *whiteView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    [self.view addSubview: whiteView];
+    self.whiteView = whiteView;
+    
+    // redView
+    UIView *redView = [[UIView alloc] initWithFrame:CGRectMake(20, 20, 100, 100)];
+    redView.backgroundColor = UIColor.redColor;
+    [self.view addSubview: redView];
+    self.redView = redView;
+    
+    // greenView
+    UIView *greenView = [[UIView alloc] initWithFrame:CGRectMake(150, 150, 150, 200)];
+    greenView.backgroundColor = UIColor.greenColor;
+    [self.view addSubview: greenView];
+    self.greenView = greenView;
+    
+    // blueView
+    UIView *blueView = [[UIView alloc] initWithFrame:CGRectMake(40, 400, 200, 150)];
+    blueView.backgroundColor = UIColor.blueColor;
+    [self.view addSubview: blueView];
+    self.blueView = blueView;
+    
+    // yellowView
+    UIView *yellowView = [[UIView alloc] initWithFrame:CGRectMake(100, 600, 180, 150)];
+    yellowView.backgroundColor = UIColor.yellowColor;
+    [self.view addSubview: yellowView];
+    self.yellowView = yellowView;
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 @end
